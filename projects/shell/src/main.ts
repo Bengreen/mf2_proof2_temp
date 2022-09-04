6f8379290821a8cee2a19074bd75827062051ae6
Ben Greene <BenJGreene@gmail.com>
2022-09-04 12:43:06 +0100

    module federation with host

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,12 +1,15 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import('./bootstrap')
+	.catch(err => console.error(err));
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-if (environment.production) {
-  enableProdMode();
-}
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
