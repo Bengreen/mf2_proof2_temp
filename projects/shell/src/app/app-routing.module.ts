78f7622f4a9f5b30cd16d54a716df185a3912a3c
Ben Greene <BenJGreene@gmail.com>
2022-09-04 10:24:20 +0100

    Add basic material home + test0

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Test0Component } from './test0/test0.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children:[
+      {
+        path: 'test0',
+        component: Test0Component,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { Test0Component } from './test0/test0.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Test0Component
   ],
   imports: [
     BrowserModule,
